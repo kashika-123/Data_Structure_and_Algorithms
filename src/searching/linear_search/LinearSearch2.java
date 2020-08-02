e9bbbe9bc585f4b217d0a52c25005e48a6909c61
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-02 14:09:03 +0530

    input of no of elemnts and array elements and element to be searched for

---
+++ src/searching/linear_search/LinearSearch2.java
@@ -0,0 +1,18 @@
+package searching.linear_search;
+
+import java.util.Scanner;
+
+public class LinearSearch2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("enter the element to be searched");
+        int element = sc.nextInt();
+    }
+}
