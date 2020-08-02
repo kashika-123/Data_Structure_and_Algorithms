c8021a44ba2441127d818358ad5ef8f112a556f9
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-02 14:28:24 +0530

    input of array and the elemet to be searched for

--- src/searching/binary_search/BinarySearch.java
+++ src/searching/binary_search/BinarySearch.java
@@ -1,4 +1,18 @@
 package searching.binary_search;
 
+import java.util.Scanner;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("enter the element to be searched");
+        int element = sc.nextInt();
+    }
 }
