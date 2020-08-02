aec2162d05d2cf640b2c336780f771a0a5ca211d
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-02 13:58:59 +0530

    input of array elements

--- src/searching/linear_search/LinearSearch1.java
+++ src/searching/linear_search/LinearSearch1.java
@@ -6,5 +6,9 @@ public class LinearSearch1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
     }
 }
