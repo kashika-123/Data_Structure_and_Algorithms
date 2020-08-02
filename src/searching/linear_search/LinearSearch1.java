525086d9ac16dd55ea3295ad681e66d51a9ad31c
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-02 13:57:52 +0530

    length of array

--- src/searching/linear_search/LinearSearch1.java
+++ src/searching/linear_search/LinearSearch1.java
@@ -1,4 +1,10 @@
 package searching.linear_search;
 
+import java.util.Scanner;
+
 public class LinearSearch1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
 }
