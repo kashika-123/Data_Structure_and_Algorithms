9a405f8280c3d508372de892818599a58e9faed6
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-02 14:06:27 +0530

    LinearSearch1 completed

--- src/searching/linear_search/LinearSearch1.java
+++ src/searching/linear_search/LinearSearch1.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class LinearSearch1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of elements");
         int n = sc.nextInt();
         int[] arr = new int[n];
+        System.out.println("enter the elements");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        System.out.println("enter the element to be searched");
         int element = sc.nextInt();
         for (int i = 0; i < n; i++) {
             if (element == arr[i])
-                System.out.println("element" + element + "found at" + i + "position");
+                System.out.println("element " + element + " found at " + i + " position");
         }
     }
 }
