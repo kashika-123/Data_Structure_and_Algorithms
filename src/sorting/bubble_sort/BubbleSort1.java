b80ad9a9032bf5de348f1e2bcdd50e18d91b6306
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-08 20:53:30 +0530

    loop from last unsorted index to to the first index of array

--- src/sorting/bubble_sort/BubbleSort1.java
+++ src/sorting/bubble_sort/BubbleSort1.java
@@ -1,5 +1,6 @@
 package sorting.bubble_sort;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BubbleSort1 {
@@ -10,11 +11,18 @@ public class BubbleSort1 {
         for (int i = 0; i < n; i++) {
             array[i] = sc.nextInt();
         }
+        for (int unsortedPartitionIndex = array.length - 1; unsortedPartitionIndex > 0; unsortedPartitionIndex--) {
 
+
+        }
+        System.out.println(Arrays.toString(array));
 
     }
 
     public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
