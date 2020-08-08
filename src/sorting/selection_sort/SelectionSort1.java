e35118e55b22aeb1e1de4a05d260843c83fc0d0f
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-08 20:59:32 +0530

    input of array elements

---
+++ src/sorting/selection_sort/SelectionSort1.java
@@ -0,0 +1,14 @@
+package sorting.selection_sort;
+
+import java.util.Scanner;
+
+public class SelectionSort1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+    }
+}
