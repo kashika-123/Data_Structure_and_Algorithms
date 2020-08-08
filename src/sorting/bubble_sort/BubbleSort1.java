46343e72b0a7afb8a8c8c2da6e5d32b01ea43e0f
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-08 20:34:02 +0530

    input of array elements

--- src/sorting/bubble_sort/BubbleSort1.java
+++ src/sorting/bubble_sort/BubbleSort1.java
@@ -1,5 +1,16 @@
 package sorting.bubble_sort;
 
+import java.util.Scanner;
+
 public class BubbleSort1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+    }
 
 }
