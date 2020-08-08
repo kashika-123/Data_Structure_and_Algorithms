7bab1f5c87d9025f5bea18ec7642c20f65436486
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-08 21:25:45 +0530

    if any of the elemnt is largest then we change its to the largets element and after the loop swap the largest with the last element

--- src/sorting/selection_sort/SelectionSort1.java
+++ src/sorting/selection_sort/SelectionSort1.java
@@ -13,10 +13,15 @@ public class SelectionSort1 {
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             int largest = 0;
             for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (array[i] > array[largest]) {
+                    largest = i;
+                }
+
             }
+            swap(array, largest, lastUnsortedIndex);
 
         }
-        //  System.out.println(Arrays.toString(array));
+        // System.out.println(Arrays.toString(array));
 
     }
 
