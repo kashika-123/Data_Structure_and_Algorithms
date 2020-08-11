778c28c87b1d7090117f48d3e508d8f822aa5f94
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-11 20:12:38 +0530

    InsertionSort1 completed

--- src/sorting/insertion_sort/InsertionSort1.java
+++ src/sorting/insertion_sort/InsertionSort1.java
@@ -1,7 +1,32 @@
 package sorting.insertion_sort;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class InsertionSort1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < n; firstUnsortedIndex++) {
+            int newElement = arr[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex - 1; i >= 0 && arr[i] > newElement; i--) {
+                arr[i + 1] = arr[i];
+            }
+            arr[i + 1] = newElement;
+            System.out.println(Arrays.toString(arr));
+
+
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
 
 
     }
---
+++ src/sorting/shell_sort/ShellSort.java
@@ -0,0 +1,7 @@
+package sorting.shell_sort;
+
+public class ShellSort {
+    public static void main(String[] args) {
+
+    }
+}
