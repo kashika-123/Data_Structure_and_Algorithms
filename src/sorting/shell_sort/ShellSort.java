6d390901eb000a6555fde376e924786fed29c469
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-08-11 20:30:23 +0530

    shellsort completed

--- src/sorting/shell_sort/ShellSort.java
+++ src/sorting/shell_sort/ShellSort.java
@@ -14,6 +14,12 @@ public class ShellSort {
         for (int gap = array.length / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < array.length; i++) {
                 int newElement = array[i];
+                int j = i;
+                while (j >= gap && array[j - gap] > newElement) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = newElement;
 
 
             }
